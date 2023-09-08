9a53f27bc6c8fcb9d9cfdf1d6cdc18fda6d70058
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2023-09-08 18:22:09 +0300

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+    const iframeEl = document.querySelector('iframe');
+const player = new Player(iframeEl);
+    
+const savedTime = localStorage.getItem('videoplayer-current-time')
+
+player.on('timeupdate', throttle( evt => {
+    localStorage.setItem('videoplayer-current-time', evt.seconds)}, 1000));
+
+
+    player.setCurrentTime(savedTime).then(function(seconds) {
+  localStorage.removeItem('videoplayer-current-time')
+    }).catch(function (error) {
+        switch (error.name) {
+            case 'RangeError':
+                break;
+
+            default:
+                break;
+        }
+    });
