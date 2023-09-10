2fc41c67d9a0f524d53575755e7f17df3483c216
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2023-09-10 10:17:38 +0300

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const selectors = {
+ formEl: document.querySelector(".feedback-form"),
+ emailEl: document.querySelector('[name="email"]'),
+ messageEl: document.querySelector('[name="message"]'),
+ submitEl: document.querySelector('button[type="submit"]')
+}
+
+
+
+
+initializeFormFields();
+
+
+selectors.formEl.addEventListener("input", throttle(setLocalStorageHandle, 500));
+
+selectors.formEl.addEventListener('submit', submitHandle )
+
+
+
+function initializeFormFields() {
+  const storedState = JSON.parse(localStorage.getItem("feedback-form-state"));
+  if (storedState) {
+    selectors.emailEl.value = storedState.email;
+    selectors.messageEl.value = storedState.message;
+  }
+}
+
+function setLocalStorageHandle() {
+    localStorage.setItem("feedback-form-state", JSON.stringify({
+    email: selectors.emailEl.value,
+    message: selectors.messageEl.value
+}));
+}
+
+function submitHandle(evt) {
+    evt.preventDefault();
+
+const storedData = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
+    console.log(storedData);
+    
+    localStorage.removeItem("feedback-form-state");
+      selectors.emailEl.value = '';
+  selectors.messageEl.value = '';
+}
